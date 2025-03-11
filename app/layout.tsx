532757f6a827b396fb48732f3ddc64e95c27af0c
Jack Chuma <jack.chuma@coinbase.com>
2025-03-11 17:03:12 -0400

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Providers } from "./providers";
 import { Inter, JetBrains_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -29,6 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} ${jetbrainsMono.variable}`}>
       <body className="bg-background antialiased">
+        <Analytics />
         <Providers>{children}</Providers>
       </body>
     </html>
